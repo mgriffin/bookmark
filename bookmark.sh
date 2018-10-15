5166c4e8df5c1082e8a8f4b448aae55bf9b1ee60
Mike Griffin <mgriffin@github.com>
2018-10-15 22:24:08 +0100

    Use pushd and popd

--- bookmark.sh
+++ bookmark.sh
@@ -13,9 +13,9 @@ DATE=$(date "+%Y-%m-%dT%H:%M:%S")
 TITLE=$(curl -s $URL | grep -io "<title>[^\<]*" | cut -f2 -d'>')
 PATH=$(basename $URL | sed 's!.*/!!' | sed '/\.html/ ! s/$/\/index.html/')
 
-cd store && /usr/local/bin/wget --force-directories --page-requisites --quiet $URL && echo "yay"
-cd ..
-pwd
+pushd store > /dev/null
+/usr/local/bin/wget --force-directories --page-requisites --quiet $URL && echo "yay"
+popd > /dev/null
 /usr/local/bin/jq --arg URL $URL --arg TITLE "$TITLE" --arg PATH $PATH --arg DATE $DATE '. += [{"url":$URL,"title":$TITLE,"path":$PATH,"date":$DATE}]' store.json > store.json.tmp && /bin/mv store.json.tmp store.json
 
 # Another option, taken from the wget manpage is to use this command
